25738237d84c5b4b9ae0127895ed6767fb6f931f
Pavan <35141851+pavantekal@users.noreply.github.com>
2024-01-01 10:53:25 +0530

    Update convertmkdwn.js

--- mkdowneditor-backend/routes/convertmkdwn.js
+++ mkdowneditor-backend/routes/convertmkdwn.js
@@ -12,10 +12,6 @@ router.use(bodyParser.json());
 
 
 
-router.get('/api/getconvertmkdwn', function(req, res, next) {
-  res.status(200).json({data: "hi tahnkyou"})
-});
-
 /* GET users listing. */
 router.post('/api/convertmkdwn', function(req, res, next) {
   const toConvertMkdwnText = req.body
@@ -23,11 +19,6 @@ router.post('/api/convertmkdwn', function(req, res, next) {
   console.log("toCOnvert",toConvertMkdwnText.text)
   const result = md.render(toConvertMkdwnText.text);
   res.status(200).json({result, success:true})
-  // if(toConvertMkdwnText.length==0) {
-  //   res.status(400).json({error:"Markdown text cannot be empty"})
-  // } else {
-  
-  // }
 });
 
 module.exports = router;
